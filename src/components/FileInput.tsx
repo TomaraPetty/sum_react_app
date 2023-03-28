22af96378201254b7f565f935da87aed3fd96e1e
Tomara Petty <tomararuth@gmail.com>
2023-03-28 00:06:24 -0700

    ADD: ignore header and first colum of csv.

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import "./fileInput.css"
+import React, { useState, useEffect } from 'react';
+import '../ui/index.css';
 
-interface FileInputProps {
-    
-}
+interface FileInputProps {}
 
 const FileInput: React.FC<FileInputProps> = (props: FileInputProps) => {
   const [file, setFile] = useState<File | undefined>(undefined);
   const [sum, setSum] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    calculateSum();
+  }, [file]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
@@ -21,32 +23,50 @@ const FileInput: React.FC<FileInputProps> = (props: FileInputProps) => {
     const lines = fileContent.split('\n');
     let total = 0;
 
-    lines.forEach((line) => {
-      const cells = line.split(',');
-      cells.forEach((cell) => {
-        const value = parseInt(cell.trim(), 10);
-        if (!isNaN(value)) {
-          total += value;
-        }
-      });
+    lines.forEach((line, index) => {
+      if (index > 0) {
+        // Exclude header
+        const cells = line.split(',');
+        cells.slice(1).forEach(cell => {
+          // Exclude first column
+          const value = parseInt(cell.trim(), 10);
+          if (!isNaN(value)) {
+            total += value;
+          }
+        });
+      }
     });
 
     setSum(total);
   };
 
   return (
-    <div>
-      <h1>Select a CSV file to upload</h1>
-      <label htmlFor="file-upload">
-        Upload CSV
-      </label>
-      <input id="file-upload" type="file" onChange={handleFileChange} />
-      <button onClick={calculateSum}>Calculate sum</button>
-      {sum !== undefined && (
-        <p>
-          Total sum: {sum}
-        </p>
-      )}
+    <div className='page'>
+      <div className='split left'>
+        <div className='wrapper'>
+          <h1 className='h1 header'>Sum The Values</h1>
+          <p className='p1'>
+            Select a CSV file to upload. We'll calculate the sum of all the
+            values in your csv.
+          </p>
+          <label htmlFor='file-upload' className='upload-button'>
+            Upload CSV
+          </label>
+          <input id='file-upload' type='file' onChange={handleFileChange} />
+        </div>
+      </div>
+      <div className='split right'>
+        <div className='wrapper'>
+          {sum !== undefined && (
+            <>
+              <p className='p1'>
+                Total of all values in your csv file:
+              </p>
+              <h1 className='h1 sum-container'>{sum}</h1>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
