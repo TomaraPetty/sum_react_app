a381a65b61901ffc229545e1a35e34e9ff23c9fd
Tomara Petty <tomararuth@gmail.com>
2023-03-27 23:58:07 -0700

    ADD: css

--- src/components/SelectCSV.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SelectCSV = (props:{onLoadFile: (files:FileList | null)=> void}) => (
-    <input
-    type="file"
-    onChange={(e:React.ChangeEvent<HTMLInputElement>) => props.onLoadFile(e.target.files)} />
-   );
-
-export default SelectCSV;
